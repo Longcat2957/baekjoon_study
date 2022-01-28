a43490a534e8ef3f7d231c9e5d6e28cccab1096f
Longcat2957 <91525066+Longcat2957@users.noreply.github.com>
2022-01-28 11:48:04 +0900

    1260 수정

--- DFS와 BFS/1260.py
+++ DFS와 BFS/1260.py
@@ -1,34 +1,50 @@
+import queue
 import sys
 N, M, V = map(int, input().split())
-visited = [False * i for i in range(N+1)]
-print(visited)
 graph = [[]* i for i in range(N+1)]
 for _ in range(M):
     a, b = map(int, sys.stdin.readline().split())
     if b not in graph[a]:
         graph[a].append(b)
-        graph[a].sort()
     if a not in graph[b]:
         graph[b].append(a)
-        graph[b].sort()
+
+for i in range(1, len(graph)):
+    graph[i].sort(reverse=True)
 # 정점의 개수 N(1<= N <= 1000)
 # 간선의 개수 M(1<= M <= 10000)
 # 정점의 번호 V
-print(graph)
 # DFS(깊이 우선 탐색)
 # 일반적으로 DFS는 스택으로 구현하며, 재귀를 이용하면 더 간단하게 구현할 수 있다.
 # 다음은 재귀를 이용한 구현 방식이다.
-ans_1 = []
+ans_dfs = []
+ans_bfs = []
 def DFS(graph:list, v:int):
-    ans_1.append(v)
-    visited[v] = True
-    if len(graph[v]) == 0 or len(ans_1) == N:
-        return
-    # 더이상 전개할 수 없을 경우 종료한다.
-    elif len(graph[v]) > 0:
-        for vertex in graph[v]:
-            if len(ans_1) != N and visited[vertex] == False:
-                DFS(graph, vertex)
+    visited = []
+    stack = [v]
+    while stack:
+        v = stack.pop()
+        if v not in visited:
+            visited.append(v)
+            print(v, end = ' ')
+            for w in graph[v]:
+                stack.append(w)
+    return visited
+
+def BFS(graph:list, v:int):
+    print(v,end=' ')
+    visited = [v]
+    from collections import deque
+    queue = [v]
+    while queue:
+        v = queue.pop(0)
+        for w in reversed(graph[v]):
+            if w not in visited:
+                visited.append(w)
+                print(w,end = ' ')
+                queue.append(w)
+    return visited
 
 DFS(graph, V)
-print(ans_1)
+print()
+BFS(graph, V)
